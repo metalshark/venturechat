50ea78b41a1b2a26487f366955f3048175c2482e
Beech Horn <beechhorn@gmail.com>
2020-03-23 16:33:28 +0000

    Rollback Database/MySQL changes to put under their own branch.

--- src/main/java/mineverse/Aust1n46/chat/database/Database.java
+++ src/main/java/mineverse/Aust1n46/chat/database/Database.java
@@ -15,11 +15,11 @@ public abstract class Database {
 
 	public abstract Connection openConnection() throws SQLException, ClassNotFoundException;
 
-	public abstract boolean checkConnection() throws Exception;
+	public abstract boolean checkConnection() throws SQLException;
 
-	public abstract Connection getConnection() throws SQLException;
+	public abstract Connection getConnection();
 
-	public abstract boolean closeConnection() throws Exception;
+	public abstract boolean closeConnection() throws SQLException;
 
 	public abstract ResultSet querySQL(String query) throws SQLException, ClassNotFoundException;
 
--- src/main/java/mineverse/Aust1n46/chat/database/MySQL.java
+++ src/main/java/mineverse/Aust1n46/chat/database/MySQL.java
@@ -1,18 +1,12 @@
 package mineverse.Aust1n46.chat.database;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import org.bukkit.plugin.Plugin;
-
-import org.apache.commons.dbcp.ConnectionFactory;
-import org.apache.commons.dbcp.DriverManagerConnectionFactory;
-import org.apache.commons.dbcp.PoolableConnectionFactory;
-import org.apache.commons.dbcp.PoolingDataSource;
-import org.apache.commons.pool.impl.GenericObjectPool;
-
-import javax.sql.DataSource;
+import mineverse.Aust1n46.chat.database.Database;
 
 //This class initializes the plugins connection to the MySQL database if it's enabled.
 public class MySQL extends Database {
@@ -21,9 +15,7 @@ public class MySQL extends Database {
 	private final String password;
 	private final String port;
 	private final String hostname;
-
-	private static GenericObjectPool pool = null;
-	private static DataSource ds = null;
+	private Connection connection;
 
 	public MySQL(Plugin plugin, String hostname, String port, String database, String username, String password) {
 		super(plugin);
@@ -32,59 +24,51 @@ public class MySQL extends Database {
 		this.database = database;
 		this.user = username;
 		this.password = password;
-	}
-
-	private String getUrl() {
-		return "jdbc:mysql://" + this.hostname + ":" + this.port + "/" + this.database + "?autoReconnect=true";
+		this.connection = null;
 	}
 
 	@Override
 	public Connection openConnection() throws SQLException, ClassNotFoundException {
-		// Creates an Instance of GenericObjectPool That Holds Our Pool of Connections Object!
-		pool = new GenericObjectPool();
-		pool.setMaxActive(4);
-
-		// Creates a ConnectionFactory Object Which Will Be Use by the Pool to Create the Connection Object!
-		ConnectionFactory cf = new DriverManagerConnectionFactory(getUrl(), user, password);
-
-		// Creates a PoolableConnectionFactory That Will Wraps the Connection Object Created by the ConnectionFactory to Add Object Pooling Functionality!
-		PoolableConnectionFactory pcf = new PoolableConnectionFactory(cf, pool, null, null, false, true);
-		ds = new PoolingDataSource(pool);
-
-		return ds.getConnection();
+		if(checkConnection()) 
+			return connection;
+		Class.forName("com.mysql.jdbc.Driver");
+		connection = DriverManager.getConnection("jdbc:mysql://" + this.hostname + ":" + this.port + "/" + this.database + "?autoReconnect=true", this.user, this.password);
+		return connection;
 	}
 
 	@Override
-	public boolean checkConnection() throws Exception {
-		try {
-			pool.borrowObject();
-		} catch (IllegalStateException e) {
+	public boolean checkConnection() throws SQLException {
+		return connection != null && !connection.isClosed();
+	}
+
+	@Override
+	public Connection getConnection() {
+		return connection;
+	}
+
+	@Override
+	public boolean closeConnection() throws SQLException {
+		if(connection == null) 
 			return false;
-		}
+		connection.close();
 		return true;
 	}
 
 	@Override
-	public Connection getConnection() throws SQLException {
-		return ds.getConnection();
+	public ResultSet querySQL(String query) throws SQLException, ClassNotFoundException {
+		if(checkConnection()) 
+			openConnection();
+		Statement statement = connection.createStatement();
+		ResultSet result = statement.executeQuery(query);
+		return result;
 	}
 
 	@Override
-	public boolean closeConnection() throws Exception {
-		if (!checkConnection()) return false;
-		pool.close();
-		return true;
-	}
-
-	@Override
-	public ResultSet querySQL(String query) throws SQLException {
-		Statement statement = getConnection().createStatement();
-		return statement.executeQuery(query);
-	}
-
-	@Override
-	public int updateSQL(String query) throws SQLException {
-		Statement statement = getConnection().createStatement();
-		return statement.executeUpdate(query);
+	public int updateSQL(String query) throws SQLException, ClassNotFoundException {
+		if(checkConnection()) 
+			openConnection();
+		Statement statement = connection.createStatement();
+		int result = statement.executeUpdate(query);
+		return result;
 	}
 }
